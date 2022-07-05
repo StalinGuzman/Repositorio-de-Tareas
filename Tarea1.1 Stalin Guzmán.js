5ab6a677240ac6dc75205af9bf783629ab3601e0
lStarkgaryenl <stalinspp@outlook.es>
2022-07-04 19:56:54 -0500

    Se agrega la tarea 1.1

---
+++ Tarea1.1 Stalin Guzmán.js
@@ -0,0 +1,15 @@
+//Clase 2
+
+var Nombre = "Stalin";
+
+    let myFunction = () =>{
+    var localOne = "Alejandro";
+    return console.log(Nombre);
+    }
+myFunction()
+console.log(localOne);
+
+//hoisting cuando las variables y funciones <=Ec5 se declaren antes que se procese cualquier tipo de codigo var, function
+
+console.log(numero);
+var numero = 10
