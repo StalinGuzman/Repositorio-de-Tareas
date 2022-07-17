a688e9a803a840552cb4f697a780db8909b13f5b
StalinGuzman <stalin.guzman@iti.edu.ec>
2022-07-17 16:13:42 -0500

    Se agrega el Foro 1

---
+++ Foro 1 Stalin Guzmán.js
@@ -0,0 +1,25 @@
+//Callback
+unction miFuncion(parámetro, callback) {
+
+    si (parámetro > 20) {
+
+        leeUnFichero(devolución de llamada);
+
+    } más {
+
+        llamar de vuelta();
+
+    }
+
+}
+//Promesas 
+promesa().luego(() => {}).luego(() => {}).luego(() => {}).luego(() => {})
+
+//Async/Away
+función asíncrona miCodigo() {
+
+    const valor1 = espera funcionAsincrona1();
+ 
+    const valor2 = espera funcionAsincrona2();
+ 
+ }
