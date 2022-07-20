07ceb3c0af8d3e975323c9144f5cff64405e00c6
StalinGuzman <stalin.guzman@iti.edu.ec>
2022-07-19 20:18:48 -0500

    Tarea 2.1 y 2.2

--- Asincronismo JavaScript/src/Tarea 2.2/Tarea 2.2 Stalin Guzmán.js
+++ Asincronismo JavaScript/src/Tarea 2.2/Tarea 2.2 Stalin Guzmán.js
@@ -3,15 +3,34 @@
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 const API = 'https://rickandmortyapi.com/api';
 
-fetch('${API}/character')
+const fetchData = (urlAPi) => {
+    return new Promise((resolved, reject) => {
+        const xhttp = new XMLHttpRequest();
+        xhttp.open("GET", urlAPi, true);
+        xhttp.onreadystatechange = (eve) => {
+            if(xhttp.readyState === 4){
+                if(xhttp.status === 200){
+                    resolved(JSON.parse(xhttp.responseText));
+                }else{
+                    const error = new Error("Error "+urlAPi)
+                    reject(error);
+                }
+            }
+        }
+        xhttp.send();
+    })
+}
+
+fetchData(`${API}/character`)
     .then(response =>{
-        return fetch('${API}/character/${response.data[1].name}')
+        console.log(response)
+        return fetchData(`${API}/character/${response.data[1].name}`)
+    })
+    /*/.then(response =>{
+        console.log(response.formData.name)
+        return fetchData (`${API}/location/${response.data.planet`})
     })
     .then(response =>{
         console.log(response.formData.name)
-        return fetch ('${API}/location/${response.data.planet}')
     })
-    .then(response =>{
-        console.log(response.formData.name)
-    })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error))/*/
